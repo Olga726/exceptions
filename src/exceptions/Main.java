fe663978dd6f4865fb62d7fd07b8b1c4343ea4f4
Olga726 <130683534+Olga726@users.noreply.github.com>
2025-09-08 13:05:00 +0500

    Update Main.java

--- src/exceptions/Main.java
+++ src/exceptions/Main.java
@@ -2,31 +2,31 @@ package exceptions;
 
 public class Main {
     public static void main(String[] args) {
-        // Напишите программу, которая пытается открыть файл с именем "data.txt".
+        // 1. Напишите программу, которая пытается открыть файл с именем "data.txt".
         // Если файл не найден, программа должна обработать исключение и вывести сообщение: "Файл не найден".
 
-        /*
+        
         try{
 
             FileReader reader = new FileReader("data.txt");
         } catch (FileNotFoundException e) {
             System.out.println("файл не найден");
         }
- */
+ 
+       //2
+       CheckedException.division(5,2);
 
-       // CheckedException.division(5,2);
-
-        /*
+       //3 
         try{
             AgeChecker.checkAge(-1);
         }catch (MyException ex){
             System.out.println(ex.getMessage());
         }
-        */
+        
+        //4
+        MailChecker.checkMail("11119");
 
-        //MailChecker.checkMail("11119");
-
-        /*
+        //5
         Box<String> stringBox = new Box<>();
         stringBox.setT("hello");
         System.out.println(stringBox.getT());
@@ -35,14 +35,15 @@ public class Main {
         integerBox.setT(123);
         System.out.println(integerBox.getT());
 
-
+        //6
         String[] stringArray = {"hello", "people"};
         GenMethod.printArray(stringArray);
 
         Integer[] intArray = { 5, 6, 77, 21};
         GenMethod.printArray(intArray);
-*/
 
+        
+        //7
         Pair<String, Integer> pair1 = new Pair<>("Яблоко: ", 420);
         System.out.println(pair1.getFirst() + pair1.getSecond());
         pair1.setFirst("Слива: ");
